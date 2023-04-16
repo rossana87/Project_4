29f72781a671f2375e200e1b320fe1f674b6b84d
Rossana87 <rossana.ventrella@gmail.com>
2023-04-16 10:24:40 +0100

    serializers created

---
+++ booking/serializers/common.py
@@ -0,0 +1,15 @@
+from rest_framework.serializers import ModelSerializer
+# import the model to serialize from
+from ..models import Booking
+
+# Create custom Serializer extending ModelSerializer that will convert our querysets into python data types and vice versa
+
+
+class BookingSerializer(ModelSerializer):
+    # This will include a Meta subclass that specifies the model we're querying and the fields we want to serialize/deserialize
+    class Meta:
+        # this is the model to query when making requests
+        model = Booking
+        # __all__ allows us to include all fields rather than specifying them individually.
+        # You can use a list or tuple of names of fields as strings if you want to be specific
+        fields = '__all__'
