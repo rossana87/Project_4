09b14b93fefddb17bbc9ff179ef7e7bf180e4d82
Rossana87 <rossana.ventrella@gmail.com>
2023-04-16 16:08:57 +0100

    small changes

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from .serializers.common import UserSerializer
+from rest_framework import status
+from rest_framework.exceptions import PermissionDenied
+import jwt
+from datetime import datetime, timedelta
+from django.conf import settings
+
+from lib.exceptions import exceptions
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 
 # Create your views here.
