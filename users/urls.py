a3ea77e56fa8eacf11174941c0dc72d196f98969
Rossana87 <rossana.ventrella@gmail.com>
2023-04-16 17:10:22 +0100

    register route works

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import RegisterView, LoginView
+from .views import RegisterView
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
-    path('login/', LoginView.as_view())
+    # path('login/', LoginView.as_view())
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers.common import UserSerializer
 from rest_framework import status
@@ -10,7 +11,20 @@ from django.conf import settings
 from lib.exceptions import exceptions
 
 from django.contrib.auth import get_user_model
+
+
 User = get_user_model()
 
 
 # Create your views here.
+class RegisterView(APIView):
+    # Register Route
+    # Endpoint: api/auth/register/
+    @exceptions
+    def post(self, request):
+        # print('REQUEST DATA -> ', request.data)
+        user_to_add = UserSerializer(data=request.data)
+        print('REQUEST DATA -> ', request.data)
+        user_to_add.is_valid(raise_exception=True)
+        user_to_add.save()
+        return Response(user_to_add.data, status.HTTP_201_CREATED)
