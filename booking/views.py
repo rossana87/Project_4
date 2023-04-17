c4c18683bf7d6909d80749a4b21f7b02cba2fbc3
Rossana87 <rossana.ventrella@gmail.com>
2023-04-17 09:40:45 +0100

    seeding files

--- booking/views.py
+++ booking/views.py
@@ -15,16 +15,16 @@ from lib.exceptions import exceptions
 class BookedClassesView(APIView):
     permission_classes = (IsAuthenticated,)
 
-    @exceptions
-    def get(self, request):
-        booking = Booking.objects.all()
-        serializer_class = BookingSerializer(booking, many=True)
-        return Response(serializer_class.data)
+    # @exceptions
+    # def get(self, request):
+    #     booking = Booking.objects.all()
+    #     serializer_class = BookingSerializer(booking, many=True)
+    #     return Response(serializer_class.data)
 
     @exceptions
     def post(self, request):
-        request.data['user'] = request.user.id
+        request.data['user_id'] = request.user.id
         booked_class = BookingSerializer(data=request.data)
         if booked_class.is_valid():
             booked_class.save()
-        return Response(booked_class.data)
+        return Response(booked_class.data, status.HTTP_202_ACCEPTED)
