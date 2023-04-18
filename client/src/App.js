882359a298919b5d9e6c2d48c34ba1aca2e6a521
Rossana87 <rossana.ventrella@gmail.com>
2023-04-18 11:01:23 +0100

    Routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,52 @@ import axios from 'axios'
 
 // Components
 import Home from './components/Home'
-
-
-
-
-
+import ClassesIndex from './components/cali/ClassesIndex'
+import PageNavbar from './components/common/PageNavbar'
+import PageNotFound from './components/common/PageNotFound'
+import Profile from './components/profile/Profile'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
 
 const App = () => {
-  useEffect(() => {
-    const getData = async () => {
-      const { data } = await axios.get('/api/cali/') // * <-- replace with your endpoint
-      console.log(data)
-    }
-    getData()
-  })
+  return (
+    <div className='site-wrapper'>
+      <BrowserRouter>
+        <PageNavbar />
 
-  return <h1>Hello World</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/classes" element={<ClassesIndex />} />
+          <Route path="/profile/:profileId" element={<Profile />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+
+          {/* The below route is rendered if nothing else matches */}
+          {/* This is best used on a 404 page */}
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+
+
+
+
+
+
+      </BrowserRouter>
+    </div>
+  )
 }
 
-export default App
+
+// const App = () => {
+//   useEffect(() => {
+//     const getData = async () => {
+//       const { data } = await axios.get('/api/cali/') // * <-- replace with your endpoint
+//       console.log(data)
+//     }
+//     getData()
+//   })
+
+//   return <h1>Hello World</h1>
+// }
+
+// export default App
