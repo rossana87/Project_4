6944bf1c6d1cad43daea76b91d7ebe8edd04457c
Rossana87 <rossana.ventrella@gmail.com>
2023-04-21 16:16:14 +0100

    profile page amends

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,6 +3,8 @@ import { useParams, Link } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import { getToken } from '../../helpers/auth'
 
+import Button from 'react-bootstrap/Button'
+
 const Profile = () => {
 
   const [error, setError] = useState('')
@@ -22,6 +24,8 @@ const Profile = () => {
           .get('/api/auth/profile/')
         setProfileData(data)
         console.log('PROFILE DATA -> ', data)
+        console.log(data.user.username)
+        console.log(data.cali_classes)
       } catch (err) {
         console.log(err)
         setError(err.message)
@@ -33,14 +37,27 @@ const Profile = () => {
 
 
 
-
   return (
     <main className="profile-container">
       <div className="classes-booked">
-
+        <h2>Classes Booked</h2>
+        <div>
+          {profileData.cali_classes.map((caliClass, i) => (
+            <div key={i} className="card-body d-flex flex-column">
+              <h4 className="card-title">{caliClass.name_class}</h4>
+              <p className="card-text">Date: {caliClass.date_class}</p>
+              <p className="card-text">Time: {caliClass.time_class}</p>
+              <p className="card-text">Duration: {caliClass.duration_class}</p>
+              <div>
+                <Button className="btn btn-dark">Cancel</Button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-      <div></div>
     </main>
   )
+
+
 }
 export default Profile
