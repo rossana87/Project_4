66c6f4addeee85204a8d535939a06b55429bf1f4
Rossana87 <rossana.ventrella@gmail.com>
2023-04-22 17:36:46 +0100

    styling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
       <div className="hero">
         <Image className="cali-top-image" src="https://res.cloudinary.com/dtu5wu4i9/image/upload/v1681843338/Project%204/a7b731_ec92dfa1ba624c5bb0f6c562f027c0fb_mv2_zixaew.webp" alt="Cali-Team" />
         <h1 className="title">
-          <span>WELCOME TO</span>
+          <span>Welcome to</span>
           <span>Cali-Kulture</span>
         </h1>
         <div className="container-header">
